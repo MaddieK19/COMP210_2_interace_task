524dac84b279c5afb7c485bcfdf605c4a02f5682
Maddie <mk186442@falmouth.ac.uk>
2016-11-17 22:16:31 +0000

    Added controller input

---
+++ COMP210_VR_interface/Assets/Scripts/Right_Input_Manager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Right_Input_Manager : MonoBehaviour {
+    private SteamVR_TrackedObject trackedObject;
+    private SteamVR_Controller.Device device;
+    public GameObject falcon;
+
+    // Use this for initialization
+    void Start()
+    {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        device = SteamVR_Controller.Input((int)trackedObject.index);
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("Trigger pressed");
+            device.TriggerHapticPulse(400);
+        }
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+
+        }
+
+
+    }
+}
